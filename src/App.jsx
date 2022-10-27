0392812fbeb4bd93d9c34d72b48e4af3d8d26b43
Adrián Bailador Panero <adrianbailador@hotmail.com>
2022-10-27 14:00:41 +0100

    useState with Array Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,25 @@ import './App.css'
 // generación de la función del componente
 const App = () => {  // Función flecha o Arrow Function
    
-    const arrayTextoFuncionModificaTexto = useState("")
+    // Array Destructuring
+    //const arrayTextoFuncionModificaTexto = useState("")
 
-    // arrayTextoFuncionModificaTexto => ["hola", funcion]
-    
+    const [stack, setStack] = useState("")
+
+    // arrayTextoFuncionModificaTexto => ["hola", funcion]    
     // 1er posicion: valor (que inicialmente es el valor por defecto)
-    const texto = arrayTextoFuncionModificaTexto[0]
-
+    //const texto = arrayTextoFuncionModificaTexto[0]
     // 2da posición: funcion que me va a permitir modificar el valor por defecto
-    const funcionModificaTexto = arrayTextoFuncionModificaTexto[1]
-
-
+    //const funcionModificaTexto = arrayTextoFuncionModificaTexto[1]
 
     // Lo que ejecuta la función
     console.log("Renderización de App")
     return (
         <main className='react-calculator'>
-            <Result value={texto}/>        
+            <Result value={stack}/>        
             <Numbers onClickNumber={number => {
             console.log("Click en number", number)
-            funcionModificaTexto(number)
+            setStack(number)
         }} />
        <Functions 
             onContentClear={() => 
